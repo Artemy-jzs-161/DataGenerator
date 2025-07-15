f0b635ff64e4a755f584a7f7f8dc708a3a76e36a
artemy-jzs-161 <borovik.artemy@yandex.ru>
2025-07-15 16:03:02 +0700

    начал делать генерацию данных для ЮЛ

--- src/main/java/org/cbr/generator/OrganizationGenerator.java
+++ src/main/java/org/cbr/generator/OrganizationGenerator.java
@@ -1,10 +1,33 @@
 package org.cbr.generator;
 
 
+import org.cbr.enums.BusinessForms;
+import org.cbr.model.OrganizationModel;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrganizationGenerator {
+    private final OrganizationNameGenerator organizationNameGenerator;
+    private final InnGenerator innGenerator;
+    private final KppGenerator kppGenerator;
+
+
+    public OrganizationGenerator(OrganizationNameGenerator organizationNameGenerator,
+                                 InnGenerator innGenerator,
+                                 KppGenerator kppGenerator) {
+        this.organizationNameGenerator = organizationNameGenerator;
+        this.innGenerator = innGenerator;
+        this.kppGenerator = kppGenerator;
+    }
+
+    public OrganizationModel generate(BusinessForms forms) {
+        OrganizationModel organizationModel = new OrganizationModel();
+        organizationModel.setName(organizationNameGenerator.generateOrganizationName(forms));
+        organizationModel.setInn(innGenerator.generateEntryInn());
+
+        return organizationModel;
+    }
+
 
 
 }
